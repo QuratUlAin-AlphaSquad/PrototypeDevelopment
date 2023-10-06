72287286ff25372440a0fbeae5a9288f99e1350a
QuratUlAin <quratulain.alphasquad@gmail.com>
2023-10-06 16:58:27 +0500

    Changes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,13 +2,34 @@
 
 import { Button, TextField } from "@mui/material";
 import { addNew, deleteSvg } from "./svg";
+import { useState } from "react";
 
 const Content = () => {
+const [data, setData] = useState([{fname:"", lname:""}])
+
+const handleClick=()=>{
+  setData([...data, {fname:"", lname:""}])
+}
+const handleChange=(e,i)=>{
+  const {name,value} = e.target
+  // console.log(e.target)
+  const onchangeVal = [...data]
+  onchangeVal[i][name]=value
+  setData(onchangeVal)
+}
+const handleDelete=(i)=>{
+  const deleteVal = [...data]
+  deleteVal.splice(i,1)
+  setData(deleteVal)
+}
   return (
     <>
       <div className="pt-[37px] pl-20 flex flex-col gap-8 max-w-[600px] w-full">
         <p className="text-base leading-4 font-medium">Categories</p>
+        
         <div className="flex items-center gap-3">
+          {/* Map Input & del btn */}
+          
           <TextField
             required
             id="outlined-basic"
@@ -24,6 +45,7 @@ const Content = () => {
           <Button
             variant="contained"
             className="!bg-[#F0F0F0] max-w-[452px] w-full"
+            // onClick={}
           >
             {addNew}
           </Button>
@@ -36,6 +58,22 @@ const Content = () => {
             </Button>
           </div>
         </div>
+      </div>
+
+
+      <div>
+        
+          {
+          data.map((val, i) =>
+          <div key={data.i}>
+            <input className="border-2" name="fname" value={val.fname} onChange={(e)=>handleChange(e,i)} />
+            {/* <input name="lname" value={val.fname} onChange={(e)=>handleChange(e,i)} /> */}
+<button onClick={()=>handleDelete(i)}>Delete</button>
+          </div>
+          )
+        }
+                  <button onClick={handleClick}> Add</button>
+
       </div>
     </>
   );
